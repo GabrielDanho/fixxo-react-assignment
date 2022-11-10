2d162ccf9015dcf837422b0e124016cc3ab6fa36
Gabriel Danho <gabriel.danho@utb.ecutbildning.se>
2022-11-10 21:21:10 +0100

    working on fixxo webapp

--- src/sections/BannersSection.js
+++ src/sections/BannersSection.js
@@ -4,11 +4,38 @@ import image2 from "../assets/images/img2.svg";
 
 const BannersSection = () => {
   return (
-    <section class="__banners container">
-        <img src={image1} alt=""/>
-        <img src={image2} alt=""/>
-   </section>
-  );
-};
+    <section class="container banner-section">
+      <div className="row">
+
+        <div className="col banner-section-background-one">
+          <div className="row">
+            <div className="col">
+              <img></img>
+            </div>
+            <div className="col">
+              <h1>Pamela Reif's Top Picks</h1>
+              <button>SHOP NOW</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col banner-section-background-two">
+          <div className="row">
+            <div className="row">
+              <div className="col">
+                <img></img>
+              </div>
+              <div className="col">
+                <h1>Let's Be Conscious</h1>
+                <button>SHOP NOW</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  )
+}
 
 export default BannersSection;
